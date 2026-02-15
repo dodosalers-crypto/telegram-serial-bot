567a43e7a149c5b62a018d80fbac301b04bf39fb
dodosalers-crypto <dodosalers@gmail.com>
2026-02-15 21:57:06 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+BOT_TOKEN = "8372696690:AAH9KM2gDUlRiDeHo8QVt9S_jO5QSUeshLE"
 
 async def test(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot working ✅")
